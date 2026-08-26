ec6a7f28ae3b3280e372a5cbd52dfa9450aa214f
ssahinfatih <fatihssahinhome@gmail.com>
2026-08-26 16:51:18 +0300

    commited

--- src/main/java/com/fatihsahin/taskmanager/mapper/TaskMapper.java
+++ src/main/java/com/fatihsahin/taskmanager/mapper/TaskMapper.java
@@ -1,35 +1,64 @@
 package com.fatihsahin.taskmanager.mapper;
 
+import com.fatihsahin.taskmanager.dto.CategoryResponse;
 import com.fatihsahin.taskmanager.dto.TaskRequest;
 import com.fatihsahin.taskmanager.dto.TaskResponse;
+import com.fatihsahin.taskmanager.entity.Category;
 import com.fatihsahin.taskmanager.entity.Task;
+import com.fatihsahin.taskmanager.service.CategoryService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskMapper {
 
-    public Task toEntity(TaskRequest request){//requseti entitye dönüştür.
-        return  Task.builder()
+    private final CategoryService  categoryService;
+
+    public TaskMapper(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    public Task toEntity(TaskRequest request) {
+        Category category = null;
+        if (request != null && request.categoryId() != null) {
+            category = categoryService.findById(request.categoryId());
+        }
+
+        return Task.builder()
                 .title(request.title())
                 .description(request.description())
                 .completed(request.completed() != null ? request.completed() : false)
+                .category(category)
                 .build();
     }
 
-    public TaskResponse toResponse(Task  task){
+    public TaskResponse toResponse(Task task) {
+        CategoryResponse categoryResponse = null;
+
+        if (task != null && task.getCategory() != null) {
+            categoryResponse = CategoryResponse.builder()
+                    .categoryId(task.getCategory().getId())
+                    .name(task.getCategory().getName())
+                    .description(task.getCategory().getDescription())
+                    .build();
+        }
+
         return TaskResponse.builder()
                 .id(task.getId())
                 .title(task.getTitle())
                 .description(task.getDescription())
-                .createdAt(task.getCreatedAt())
                 .completed(task.getCompleted())
+                .createdAt(task.getCreatedAt())
+                .category(categoryResponse)
                 .build();
     }
 
-    public void updateEntityFromRequest(Task task,TaskRequest request){
+    public void updateEntityFromRequest(Task task, TaskRequest request) {
         task.setTitle(request.title());
         task.setDescription(request.description());
         task.setCompleted(request.completed());
 
+        if (request.categoryId() != null) {
+            task.setCategory(categoryService.findById(request.categoryId()));
+        }
     }
 }
