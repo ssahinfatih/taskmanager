c599ae7e090ba47c6433d815d180c53afa999ac8
ssahinfatih <fatihssahinhome@gmail.com>
2026-08-26 16:45:49 +0300

    26.08.2026 15:26

--- src/main/java/com/fatihsahin/taskmanager/entity/Category.java
+++ src/main/java/com/fatihsahin/taskmanager/entity/Category.java
@@ -4,19 +4,19 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Entity
-@Table(name = "category")
+@Table(name = "categories")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "name", nullable = false,unique = true)
+
+    @Column(nullable = false, unique = true)
     private String name;
+
     @Column(length = 255)
     private String description;
 }
--- src/main/java/com/fatihsahin/taskmanager/entity/Task.java
+++ src/main/java/com/fatihsahin/taskmanager/entity/Task.java
@@ -1,8 +1,15 @@
 package com.fatihsahin.taskmanager.entity;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +18,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "task")
+@Table(name = "tasks")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
